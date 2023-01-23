bc9b56be462c816c349b741356b85f2fc78dbda0
cexperto <sipuedeshazloya@gmail.com>
2023-01-23 10:17:48 -0500

    cli, requirements

--- randomnumber/views.py
+++ randomnumber/views.py
@@ -10,8 +10,10 @@ def random_number(request):
     if request.method == "POST":
         n1 = request.data["numberone"]
         n2 = request.data["numbertwo"]
+        if n1 == 0:
+            n1 = 1
         if n1 and n2 and n1 < n2:
-            url = f"https://www.randomnumberapi.com/api/v1.0/random?min={1}&max={n2}&count=1"
+            url = f"https://www.randomnumberapi.com/api/v1.0/random?min={n1}&max={n2}&count=1"
             r = requests.get(url)
             return Response({"number": r})
         return Response({"error": "numbertwo must be > numberone"})
